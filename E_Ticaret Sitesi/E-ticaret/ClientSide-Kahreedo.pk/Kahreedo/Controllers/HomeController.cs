8e3a62ebfbea9583f2ab511b12183b251834470c
furknsyl <fsoylu500@gmail.com>
2024-06-09 23:51:35 +0300

    E_Ticaret Sitesi
    Asp.Net MVC ve SQL kullanıldı

---
+++ E_Ticaret Sitesi/E-ticaret/ClientSide-Kahreedo.pk/Kahreedo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Khareedo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Khareedo.Controllers
+{
+    public class HomeController : Controller
+    {
+        KhareedoEntities db = new KhareedoEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.utu = db.Products.Where(x => x.Category.Name.Equals("Ütü")).ToList();
+            ViewBag.kahve = db.Products.Where(x => x.Category.Name.Equals("Kahve")).ToList();
+            ViewBag.blender = db.Products.Where(x => x.Category.Name.Equals("Blender")).ToList();
+            ViewBag.mikrodalgafirin = db.Products.Where(x => x.Category.Name.Equals("Mikrodalga Fırın")).ToList();
+            ViewBag.esupurge = db.Products.Where(x => x.Category.Name.Equals("Elektrikli Süpürge")).ToList();
+            ViewBag.tmakinesi = db.Products.Where(x => x.Category.Name.Equals("Tost Makinesi")).ToList();
+            ViewBag.Slider = db.genMainSliders.ToList();
+            ViewBag.PromoRight = db.genPromoRights.ToList();
+
+            this.GetDefaultData();
+
+            return View();
+        }      
+
+    }
+}
